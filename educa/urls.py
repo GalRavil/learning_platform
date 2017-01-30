f5f11b1fc1c61108015994cbfeef67506de9c43b
GalRavil <gal.ravil93@gmail.com>
2017-01-30 20:06:52 +0300

    + urls for CourseListView & CourseDetailView

--- educa/urls.py
+++ educa/urls.py
@@ -17,12 +17,15 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
+from courses.views import CourseListView
+
 
 urlpatterns = [
     url(r'account/login/$', auth_views.login, name='login'),
     url(r'account/logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^course/', include('courses.urls')),
+    url(r'^$', CourseListView.as_view(), name='course_list'),
 ]
 
 if settings.DEBUG:
