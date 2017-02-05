a7f86f8d6ffe0616121d09093406c54ce48ea192
GalRavil <gal.ravil93@gmail.com>
2017-02-05 13:22:30 +0300

    + rendering different types of content

--- educa/settings.py
+++ educa/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'debug_toolbar',
     'students',
+    'embed_video',
 )
 
 MIDDLEWARE_CLASSES = (
--- educa/urls.py
+++ educa/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
@@ -28,6 +29,9 @@ urlpatterns = [
     url(r'students/', include('students.urls')),
     url(r'^$', CourseListView.as_view(), name='course_list'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
