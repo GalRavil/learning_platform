1ef41ce0ec2acd7a6ec71d59df529802d59b8607
GalRavil <gal.ravil93@gmail.com>
2017-02-05 09:23:58 +0300

    + enrolling in courses

--- students/views.py
+++ students/views.py
@@ -1,7 +1,11 @@
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, FormView
+
+from braces.views import LoginRequiredMixin
+
+from .forms import CourseEnrollForm
 
 
 class StudentRegistrationView(CreateView):
@@ -16,3 +20,17 @@ class StudentRegistrationView(CreateView):
                             password=cd['password'])
         login(self.request, user)
         return result
+
+
+class StudentEnrollCourseView(LoginRequiredMixin, FormView):
+    course = None
+    form_class = CourseEnrollForm
+
+    def form_valid(self, form):
+        self.course = form.cleaned_data['course']
+        self.course.students.add(self.request.user)
+        return super(StudentEnrollCourseView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('student_course_detail',
+                            args=[self.course.id])
