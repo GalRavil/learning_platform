2fb51e363abd9746352f641620e90b4f0999162b
GalRavil <gal.ravil93@gmail.com>
2017-02-11 13:42:19 +0300

    + api: CourseEnrollView

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,12 @@
-from rest_framework import generics
+from django.shortcuts import get_object_or_404
 
-from ..models import Subject
+from rest_framework import generics
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from ..models import Subject, Course
 from .serializers import SubjectSerializer
 
 
@@ -12,3 +18,17 @@ class SubjectListView(generics.ListAPIView):
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+
+# view for users to enroll in courses
+class CourseEnrollView(APIView):
+    # user and password are sent by the client in the Authorization HTTP
+    # header encoded with Base64.
+    authentication_classes = (BasicAuthentication,)
+    # Allows access to authenticated users only
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, pk, format=None):   # only POST method allowed
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
